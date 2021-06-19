4687890c695bf6fe486bbf906a6ada7554989d7f
Martina <martinaescorial9@gmail.com>
2021-06-19 15:42:26 +0200

    Add project files.

---
+++ UD4E21/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UD4E21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String dia = "Introduce un dia de la semana";
+
+            switch (dia)
+            {
+                case "lunes":
+                case "martes":
+                case "miercoles":
+                case "jueves":
+                case "viernes":
+                    Console.WriteLine("Es un dia laboral");
+                    break;
+                case "sabado":
+                case "domingo":
+                    Console.WriteLine("Es un dia festivo");
+                    goto default;
+                default:
+                    Console.WriteLine("Introduce un dia de la semana");
+                    break;
+            }
+        }
+    }
+}
